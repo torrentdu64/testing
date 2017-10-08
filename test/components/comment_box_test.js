7afe4abc7e7bd795815f675e2f9d01f2faab9823
torrent benoit <torrentdu64@gmail.com>
2017-10-08 16:07:47 +0200

    use jquery to  find element

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -2,6 +2,13 @@ import { renderComponent, expect} from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 
-describe( '', () => {
+describe( 'CommentBox', () => {
+    it( 'has text area', () => {
+      const component = renderComponent(CommentBox);
+        expect(component.find('textarea')).to.exist;
+    });
+    it('has a botton', () => {
+
+    });
 
 });
