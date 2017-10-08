2e18625bc106d37c53b0e671e10c27b452e1d695
torrent benoit <torrentdu64@gmail.com>
2017-10-08 16:29:45 +0200

    refacto let component and beforeEach

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -3,16 +3,18 @@ import CommentBox from '../../src/components/comment_box';
 
 // google doc chai jquery
 describe( 'CommentBox', () => {
+  let component;
+  beforeEach(() => {
+    component = renderComponent(CommentBox);
+  });
+
   it('has correct class', () => {
-    const component = renderComponent(CommentBox);
     expect(component).to.have.class('comment-box');
   });
     it( 'has text area', () => {
-      const component = renderComponent(CommentBox);
         expect(component.find('textarea')).to.exist;
     });
     it('has a botton', () => {
-      const component = renderComponent(CommentBox);
       expect(component.find('button')).to.exist;
     });
 
