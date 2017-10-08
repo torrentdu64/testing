6d667fbbe4111c3cd2235cd6fb78a23da9842401
torrent benoit <torrentdu64@gmail.com>
2017-10-08 16:21:50 +0200

    create component

---
+++ src/components/comment_box.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+class CommentBox extends Component {
+  render() {
+    return (
+      <div className="comment-box">
+      <textarea  >
+      </textarea>
+      <button>Submit comment</button>
+      </div>
+      );
+  }
+}
+
+export default CommentBox;
--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -1,14 +1,19 @@
 import { renderComponent, expect} from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
-
+// google doc chai jquery
 describe( 'CommentBox', () => {
+  it('has correct class', () => {
+    const component = renderComponent(CommentBox);
+    expect(component).to.have.class('comment-box');
+  });
     it( 'has text area', () => {
       const component = renderComponent(CommentBox);
         expect(component.find('textarea')).to.exist;
     });
     it('has a botton', () => {
-
+      const component = renderComponent(CommentBox);
+      expect(component.find('button')).to.exist;
     });
 
 });
