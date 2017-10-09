00fe9f4a2d9b9cbe0bbb33b793fa077fded982e8
torrent benoit <torrentdu64@gmail.com>
2017-10-09 16:12:40 +0200

    li expect

--- test/components/comment_list_test.js
+++ test/components/comment_list_test.js
@@ -3,5 +3,18 @@ import CommentList from '../../src/components/comment_list';
 
 
 describe('CommentList', () => {
+    let component;
 
+    beforeEach(() => {
+      const props =  {comments: [ 'new comment', 'other new comment']}
+      component = renderComponent(CommentList, null, props);
+    });
+
+    it('shows a LI for each comment', () => {
+        expect(component.find('li').length).to.equal(2);
+    });
+    it('shows each comment that is provided', () => {
+        expect(component).to.contain('new comment');
+        expect(component).to.contain('other new comment');
+    });
 });
