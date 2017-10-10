b584f8424c5cfa017782a9a621b935cd1fe42846
torrent benoit <torrentdu64@gmail.com>
2017-10-10 11:16:03 +0200

    add provider

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,6 +2,11 @@ import jsdom from 'jsdom';
 import jquery from 'jquery';
 import TestUtils from 'react-addons-test-utils';
 import ReactDOM from 'react-dom';
+import {expect} from 'chai';
+import React from 'react';
+import {Provider} from 'react-redux';
+import { createStore} from 'redux';
+import reducers from '../src/reducers';
 
 
 
@@ -10,6 +15,12 @@ global.window = global.document.defauldView;
 const $ = jquery(global.window);
 
 function renderComponent(ComponentClass) {
-  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+  const componentInstance = TestUtils.renderIntoDocument(
+    <Provider store={createStore(reducers)}>
+    <ComponentClass />
+    </Provider>);
   return $(ReactDOM.findDOMNode(componentInstance));
 }
+
+
+export { renderComponent, expect};
